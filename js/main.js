ec5ac4e6a662a5a0812cd63501341c9298d2acb7
Belhenniche Yassaa <Yagon66@gmail.com>
2019-03-13 22:06:27 +0100

    feat: added MapBox API Key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoieWFnb24iLCJhIjoiY2p0N28yd2VuMDA0dTN5bDR3NWV1eTV0ZiJ9.ZEm28_EpnoQ_kuDR-mm1fg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
